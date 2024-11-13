6425337be40d57f6aafee50fc3fffbf70f64c573
Geisel <gpachecome@unsa.edu.pe>
2024-11-13 13:15:11 -0500

    primera version

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente{
+    private String nombre;
+    private String apaellido;
+
+    public void setApaellido(String apaellido) {
+        this.apaellido = apaellido;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
